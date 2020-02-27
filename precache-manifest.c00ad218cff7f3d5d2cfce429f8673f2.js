4a8da351726abddf45af2448355d916b26f7a472
selimaakid1 <selima.akid@yahoo.com>
2020-02-27 15:38:45 +0100

    Updates

---
+++ precache-manifest.c00ad218cff7f3d5d2cfce429f8673f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf2ae070ad89dfd11a95bf45fbf6dee6",
+    "url": "/Nav_Menu/index.html"
+  },
+  {
+    "revision": "e21fc8c0597854193e08",
+    "url": "/Nav_Menu/static/css/main.f7697683.chunk.css"
+  },
+  {
+    "revision": "2a56ee702ec40caa7655",
+    "url": "/Nav_Menu/static/js/2.e5c3ae8e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Nav_Menu/static/js/2.e5c3ae8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e21fc8c0597854193e08",
+    "url": "/Nav_Menu/static/js/main.3110032f.chunk.js"
+  },
+  {
+    "revision": "c73b469549684085753f",
+    "url": "/Nav_Menu/static/js/runtime-main.ca081c1c.js"
+  }
+]);
